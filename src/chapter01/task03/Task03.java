97527092a2b182b3be74098416ad4a6b0e2d7b55
Denis <dnb2011@yandex.ru>
2024-10-19 13:46:13 +0300

    feat: chapter01.task03

---
+++ src/chapter01/task03/Task03.java
@@ -0,0 +1,8 @@
+package chapter01.task03;
+
+public class Task03 {
+    public static void main(String[] args) {
+        SolutionTask03 solutionTask03 = new SolutionTask03();
+        solutionTask03.getMaxNumber(solutionTask03.readDataFromConsole());
+    }
+}
